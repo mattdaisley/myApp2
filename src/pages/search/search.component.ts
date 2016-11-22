7cd16e1e52d4d88401186c935543f7cb5df202f4
Matt Daisley <mattdaisley@gmail.com>
2016-11-22 14:17:50 -0700

    mockup of search page

--- src/pages/search/search.component.ts
+++ src/pages/search/search.component.ts
@@ -5,29 +5,30 @@ import { NavController, NavParams, Searchbar } from 'ionic-angular';
 import { ItemDetailsPage } from '../';
 
 @Component({
+  selector: 'search-page',
   templateUrl: 'search.html'
 })
 export class SearchPage {
 
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  nearby: Array<{title: string, address: string, id: number}>;
+  recent: Array<{title: string, address: string, id: number}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
 
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.nearby = [
+      { title: 'First person', address: '123 first st', id: 1 },
+      { title: 'Second person', address: '345 second dr', id: 2 },
+      { title: 'Third person', address: '678 third pl', id: 3 },
+      { title: 'Fourth person', address: '910 fourth circle', id: 4 }
+    ];
 
-    this.items = [];
-    for(let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    this.recent = [
+      { title: 'Recent First person', address: '123 first st', id: 4 },
+      { title: 'Recent Second person', address: '345 second dr', id: 5 }
+    ];
   }
 
   itemTapped(event, item) {
